a809167699809d155b57dfd52cf8fb952616e4e1
bardrabbit <bardrabbit@gmail.com>
2021-08-10 17:50:47 -0400

    add bottom header

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,9 +47,21 @@ const Header = () => {
           </div>
         </div>
       </div>
-
       {/* bottom nav */}
-      <div></div>
+      <div className="flex items-center bg-amazon_blue-light text-white text-sm space-x-3 p-2 pl-6">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Today's Deals </p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </header>
   );
 };
